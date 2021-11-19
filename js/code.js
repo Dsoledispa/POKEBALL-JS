ae067ba6629834bc5d81aa29893b95060a3bad37
Diego <diegojavi2026@gmail.com>
2021-11-19 19:34:20 +0100

    E2 hecho

--- js/code.js
+++ js/code.js
@@ -1,18 +1,22 @@
-let salud = document.getElementById("saludo").value;
-
 window.onload = function() {
     saludo();
-    console.log(salud);
+
 }
 
 function saludo() {
+    let salud = document.getElementById("saludo");
+    let hoy = new Date();
+    let hora = hoy.getHours();
     if (hora < 7 || hora > 17) {
         //noche
-        document.body.style.backgroundColor = "red";
+        document.body.style.backgroundColor = "#3E3730";
+        document.body.style.color = "white";
+        salud.innerHTML = "¡Buenas noches!";
 
     } else {
-
-        //día
+        //dia
+        document.body.style.backgroundColor = "rgb(238, 107, 47)";
+        salud.innerHTML = "¡Buenos días!";
 
     }
 
